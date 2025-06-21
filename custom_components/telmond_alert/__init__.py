c4b70adf0c8eb6030087375c59e044e1f35a2962
elishalight <87694147+elishalight@users.noreply.github.com>
2025-06-21 21:12:40 +0300

    Update __init__.py

--- custom_components/telmond_alert/__init__.py
+++ custom_components/telmond_alert/__init__.py
@@ -1,1 +1,17 @@
-"""Tel Mond Red Alert By Elisha Design - Init."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    # no YAML config support
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    # Set up your integration here after UI setup
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return True
