8b017568f973df3c147aee5442f2ebd82ec1b5f9
elishalight <87694147+elishalight@users.noreply.github.com>
2025-06-21 20:59:08 +0300

    Create const.py

---
+++ custom_components/telmond_alert/const.py
@@ -0,0 +1,8 @@
+DOMAIN = "telmond_alert"
+TARGET_CITY = "תל מונד"
+URL = "https://www.oref.org.il/WarningMessages/alert/alerts.json"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://www.oref.org.il/",
+    "X-Requested-With": "XMLHttpRequest"
+}
